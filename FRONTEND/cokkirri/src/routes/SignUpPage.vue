5577bfb135cb8fb20d785af8f69d5971106b3656
kshoon99 <kshun08@dongguk.edu>
2023-05-12 01:07:47 +0900

    axios setting & 회원가입 페이지 api 연동

--- FRONTEND/cokkirri/src/routes/SignUpPage.vue
+++ FRONTEND/cokkirri/src/routes/SignUpPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- 이곳에 회원가입 뼈대 생성. 추후 컴포넌트로 빼내서 활용할 예정.  -->
+    <!-- 회원가입 -->
     <div class="container" >
         <div class="form-frame">
             <div :style="{'margin-left': '341px'}">
@@ -9,7 +9,7 @@
                 <router-link to="/" :style="{'margin-left': '95px','color': '#B87514'}">로그인</router-link>
             </div>
             <h2 :style="{'padding-top': '10px','margin-left': '44px'}">회원가입</h2>
-            <p :style="{'margin-top': '52px','margin-left': '44px','margin-bottom': '0px'}"><span :style="{'font-size':'22px',color:'red'}">*</span>동국대 웹메일 주소를 입력하세요</p>
+            <p :style="{'margin-top': '52px','margin-left': '44px','margin-bottom': '0px'}">동국대 웹메일 주소를 입력하세요</p>
             <input
                 name="emailAddress" 
                 placeholder="email address"
@@ -25,7 +25,7 @@
                 v-if="formControl.isErrorEmail"
                 :style="{'margin-left':'44px','color':'red'}">
                 동국대 웹메일만 가능합니다. 잘못된 입력입니다.</p>
-            <p :style="{'margin-top': '35px','margin-left': '44px','margin-bottom': '0px'}"><span :style="{'font-size':'22px',color:'red'}">*</span>비밀번호</p>
+            <p :style="{'margin-top': '35px','margin-left': '44px','margin-bottom': '0px'}">비밀번호</p>
             <input 
                 type="password"
                 name="password" 
@@ -34,7 +34,7 @@
                 @keydown.enter="returnPasswordAgainValidationResult"
                 @change="returnPasswordAgainValidationResult"
                 v-model="member.userPassword" >
-            <p :style="queryStyle"><span :style="{'font-size':'22px',color:'red'}">*</span>재확인</p>
+            <p :style="queryStyle">재확인</p>
             <input
                 type="password"
                 placeholder="password again"
@@ -52,26 +52,11 @@
                 :style="{'margin-left':'44px','color':'red'}">
                 비밀번호가 일치하지 않습니다.</p>
             </div>
-            <p :style="[queryStyle,{'margin-top':'35px'}]"><span :style="{'font-size':'22px',color:'red'}">*</span>학번</p>
-            <input
-                placeholder="student ID"
-                :style="[bigInputStyle,{'margin-bottom':'17px'}]" 
-                v-model="member.userStudentId">
-            <p :style="queryStyle">이름</p>
+            <p :style="[queryStyle,{'margin-top':'35px'}]">이름</p>
             <input
                 placeholder="name"
                 :style="bigInputStyle" 
                 v-model="member.userName">
-            <p :style="queryStyle">닉네임</p>
-            <input
-                placeholder="nickname"
-                :style="bigInputStyle" 
-                v-model="member.userNickname">
-            <p :style="queryStyle">학과</p>
-            <input
-                placeholder="소속"
-                :style="bigInputStyle" 
-                v-model="member.userDepartment">
             <p :style="queryStyle">성별</p>
             <input
                 type="radio"
@@ -85,11 +70,21 @@
                 :style="{'margin-left': '10px'}" 
                 value="female" v-model="member.userSex">
                 <label for="female">여자</label>
-            <p :style="queryStyle">나이</p>
-            <input 
-                placeholder="age"
+            <p :style="queryStyle">학과</p>
+            <input
+                placeholder="major"
                 :style="bigInputStyle" 
-                v-model="member.userAge">
+                v-model="member.userMajor">
+            <p :style="queryStyle">휴대폰 번호</p>
+            <input
+                placeholder="phone number"
+                :style="bigInputStyle" 
+                v-model="member.userPhoneNumber">
+            <p :style="queryStyle">학번</p>
+            <input
+                placeholder="student ID"
+                :style="[bigInputStyle,{'margin-bottom':'17px'}]" 
+                v-model="member.userStudentId">
             <button 
                 @click= "submitForm"
                 :style="ButtonStyle">
@@ -100,21 +95,32 @@
 </template>
 
 <script>
+// axios 객체 불러오기. 해당 객체에 내포된 baseURL을 사용하기 위해서다.
+import axios from '../api/index.js';
+
 export default {
     // back-end의 API와 연결 필요함
     data() {
         return {
             // 회원가입 유저의 입력 폼
             member: {
+                // id
                 userEmailAddress: '',
+                // password
                 userPassword: '',
                 userPasswordAgain: '',
-                userStudentId: '',
+                // name
                 userName: '',
-                userNickname: '',
-                userDepartment: '',
+                 // sex(성별)
                 userSex: '',
-                userAge: ''
+                 // major 변수 이름 수정 필요
+                userMajor: '',
+                // number 핸드폰 번호
+                userPhoneNumber: '',
+                // studentNum
+                userStudentId: '',
+                // course 추후에 user Course 추가 가능성 염두
+                // userCourse: ''
             },
             // 유효성 상태 표시
             formControl:    {
@@ -155,7 +161,7 @@ export default {
     methods: {
         // 이메일 유효성 판단
         returnEmailValidationResults(){
-            if(this.member.userEmailAddress.endsWith('@dongguck.edu') | this.member.userEmailAddress.endsWith('@dgu.ac.kr')){
+            if(this.member.userEmailAddress.endsWith('@dongguk.edu') | this.member.userEmailAddress.endsWith('@dgu.ac.kr')){
                 this.formControl.isGoodEmail = true
                 this.formControl.isErrorEmail = false
             }
@@ -178,20 +184,48 @@ export default {
         turnOnPasswordAgainValidation(){
             this.formControl.onPasswordAgainValidation = true
         },
-        // 작성한 폼 제출하는 부분. back-end의 API와 연결 필요함
+        // 회원 가입 api로 post 요청
+        async signUp() {
+            try{
+                await axios.post('/signup', null, {
+                    params: {
+                        id: this.member.userEmailAddress,
+                        password: this.member.userPassword,
+                        name: this.member.userName,
+                        sex: this.member.userSex,
+                        major: this.member.userMajor,
+                        number:this.member.userPhoneNumber,
+                        studentNum: this.member.userStudentId,
+                        // course: "CSE4051-01"
+                    }
+                })
+                .then((result) => {
+                    console.log(result.status);
+                    if(result.status === 200){
+                        alert(this.member.userEmailAddress+"(으)로 인증 메일을 보냈습니다.")
+                        this.$router.replace('/');
+                    }
+                    else{
+                        alert("reponse state가 200이 아닙니다.")
+                    }
+                })
+                .catch(function(error){
+                    console.log(error);
+                })
+            } catch(error){
+                console.log(error)
+            }
+        },
+        // 계정, 비밀번호에 대한 유효성 검사 고려. 이외에는 빈칸만 제한 적용.
         submitForm(){
-            if(this.formControl.isGoodEmail & !(this.member.userPassword==='') & this.formControl.isGoodPasswordAgain & !(this.member.userStudentId=='')){
-                alert(this.member.userEmailAddress+"(으)로 인증 메일을 보냈습니다.")
-                this.$router.replace('/')
+            if(this.formControl.isGoodEmail & !(this.member.userPassword==='') & this.formControl.isGoodPasswordAgain & !(this.member.userName=='') & !(this.member.userSex=='') & !(this.member.userMajor=='') & !(this.member.userPhoneNumber=='') & !(this.member.userStudentId=='')){
+                this.signUp()
             }
             else{
                 alert("잘못된 입력입니다.")
             }
-        }
+        },
     },
-    watch: {
-
-    }
 }
 </script>
 

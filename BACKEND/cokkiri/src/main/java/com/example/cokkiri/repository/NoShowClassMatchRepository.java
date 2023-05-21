46f7bfb1a9c07064097e47325d72540b1b942af4
gsandoo <rogersddt@naver.com>
2023-05-22 01:50:03 +0900

    매칭 완료버튼 기능 추가, 신고 작성, 내역 조회 추가, 노쇼 기능 구현 중

---
+++ BACKEND/cokkiri/src/main/java/com/example/cokkiri/repository/NoShowClassMatchRepository.java
@@ -0,0 +1,8 @@
+package com.example.cokkiri.repository;
+
+import com.example.cokkiri.model.NoShowClassMatchList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoShowClassMatchRepository extends JpaRepository<NoShowClassMatchList,String> {
+
+}

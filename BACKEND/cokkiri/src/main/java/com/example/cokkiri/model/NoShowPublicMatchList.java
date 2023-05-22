bd8b1b28580f2d2101cb55704851a94bbb7c523a
gsandoo <rogersddt@naver.com>
2023-05-22 23:59:50 +0900

    fix: 매칭 등록, 매칭 조회, 신고 조회, 신고 등록 기능 fix

--- BACKEND/cokkiri/src/main/java/com/example/cokkiri/model/NoShowPublicMatchList.java
+++ BACKEND/cokkiri/src/main/java/com/example/cokkiri/model/NoShowPublicMatchList.java
@@ -1,10 +1,8 @@
 package com.example.cokkiri.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.util.List;
 

2cc5e0197e3065ac558d33f5761e2c0d5ea5bc37
gsandoo <rogersddt@naver.com>
2023-06-02 19:16:36 +0900

    Fix: 매칭 기능 수정

--- BACKEND/cokkiri/src/main/java/com/example/cokkiri/repository/ClassMatchingWaitRepository.java
+++ BACKEND/cokkiri/src/main/java/com/example/cokkiri/repository/ClassMatchingWaitRepository.java
@@ -5,9 +5,10 @@ import com.example.cokkiri.model.PublicMatchingWait;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ClassMatchingWaitRepository extends JpaRepository<ClassMatchingWait , Integer> {
 
-    public List<ClassMatchingWait> findByEmail(String id);
+    public Optional<ClassMatchingWait> findByEmail(String id);
     public  ClassMatchingWait findById(int id);
 }
--- BACKEND/cokkiri/src/main/java/com/example/cokkiri/repository/PublicMatchingWaitRepository.java
+++ BACKEND/cokkiri/src/main/java/com/example/cokkiri/repository/PublicMatchingWaitRepository.java
@@ -4,8 +4,10 @@ import com.example.cokkiri.model.PublicMatchingWait;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PublicMatchingWaitRepository extends JpaRepository<PublicMatchingWait, Integer> {
-    public List<PublicMatchingWait> findByEmail(String id);
+    public Optional<PublicMatchingWait> findByEmail(String id);
     public PublicMatchingWait findById(int id);
+
 }

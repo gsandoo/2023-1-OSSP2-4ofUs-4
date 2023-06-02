2477fcb42f42d33e034de66a9fcb1b75fe20da2a
gsandoo <rogersddt@naver.com>
2023-06-02 19:57:01 +0900

    Fix: 노쇼 취소 기능 타입별  취소

--- BACKEND/cokkiri/src/main/java/com/example/cokkiri/repository/NoShowClassMatchListRepository.java
+++ BACKEND/cokkiri/src/main/java/com/example/cokkiri/repository/NoShowClassMatchListRepository.java
@@ -4,5 +4,5 @@ import com.example.cokkiri.model.NoShowClassMatchList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoShowClassMatchListRepository extends JpaRepository<NoShowClassMatchList,Integer> {
-
+    public  NoShowClassMatchList findByEmail(String id);
 }
